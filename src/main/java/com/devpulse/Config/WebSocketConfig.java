a4779151ee147bb1d61214cdf2d5392d092e9a8d
Avnish666 <avanishsingh9228@gmail.com>
2026-07-02 19:06:22 +0530

    web socket security increased

--- src/main/java/com/devpulse/Config/WebSocketConfig.java
+++ src/main/java/com/devpulse/Config/WebSocketConfig.java
@@ -1,6 +1,6 @@
 package com.devpulse.Config;
 
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -9,25 +9,26 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig
-        implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Value("${app.frontend-url}")
+    private String frontendUrl;
 
     @Override
-    public void configureMessageBroker(
-            MessageBrokerRegistry config) {
+    public void configureMessageBroker(MessageBrokerRegistry config) {
 
         config.enableSimpleBroker("/topic");
-
         config.setApplicationDestinationPrefixes("/app");
     }
-//commit
-    @Override
-    public void registerStompEndpoints(
-            StompEndpointRegistry registry) {
 
-        registry
-                .addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins(
+                        "http://localhost:5173",
+                        frontendUrl
+                )
                 .withSockJS();
     }
 }
